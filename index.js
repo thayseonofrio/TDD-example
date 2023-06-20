ecba267ffa68b4fcab64bd5091c46b16a64f13cd
Thayse <thayseonofrio@hotmail.com>
2023-06-20 17:52:03 -0300

    feat: fizz buzz for input until 15

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 
 const isMultipleOfThree = (number) => number % 3 === 0
 
+const isMultipleOfFive = (number) => number % 5 === 0
+
 export const getFizzBuzzResult = (input) => {
     const result = input.map(item => {
+        if (isMultipleOfThree(item) && isMultipleOfFive(item)) {
+            return "FizzBuzz"
+        }
         if (isMultipleOfThree(item)) {
             return "Fizz"
+        }
+        if (isMultipleOfFive(item)) {
+            return "Buzz"
         }
         return item
     });
--- test.js
+++ test.js
@@ -1,18 +1,29 @@
-import assert from "node:assert"
-import {it, describe} from "node:test"
-import {getFizzBuzzResult} from "./index.js"
+import assert from "node:assert";
+import { it, describe } from "node:test";
+import { getFizzBuzzResult } from "./index.js";
 
 describe("TDD example", () => {
-    it("should get correct FizzBuzz result for input 1", () => {
-        const input = [1]
-        const result = getFizzBuzzResult(input)
-        assert.deepStrictEqual(result, "1")
-    })
+  it("should get correct FizzBuzz result for input 1", () => {
+    const input = [1];
+    const result = getFizzBuzzResult(input);
+    assert.deepStrictEqual(result, "1");
+  });
 
-    it("should get correct FizzBuzz result for input 1, 2, 3", () => {
-        const input = [1, 2, 3]
-        const result = getFizzBuzzResult(input)
-        assert.deepStrictEqual(result, "1,2,Fizz")
-    })
-})
+  it("should get correct FizzBuzz result for input 1, 2, 3", () => {
+    const input = [1, 2, 3];
+    const result = getFizzBuzzResult(input);
+    assert.deepStrictEqual(result, "1,2,Fizz");
+  });
 
+  it("should get correct FizzBuzz result for input 1, 2, 3, 4, 5", () => {
+    const input = [1, 2, 3, 4, 5];
+    const result = getFizzBuzzResult(input);
+    assert.deepStrictEqual(result, "1,2,Fizz,4,Buzz");
+  });
+
+  it("should get correct FizzBuzz result for input 1, 2, 3, 4, 5, 10, 15", () => {
+    const input = [1, 2, 3, 4, 5, 10, 15];
+    const result = getFizzBuzzResult(input);
+    assert.deepStrictEqual(result, "1,2,Fizz,4,Buzz,Buzz,FizzBuzz");
+  });
+});
