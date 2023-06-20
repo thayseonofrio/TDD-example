e59418975f4c3f6a53cc9b0dcd2d53f6242956e8
Thayse <thayseonofrio@hotmail.com>
2023-06-20 17:21:29 -0300

    feat: fizz buzz for input 1

---
+++ index.js
@@ -0,0 +1,3 @@
+export const getFizzBuzzResult = (input) => {
+    return input.toString();
+}
--- test.js
+++ test.js
@@ -1,9 +1,12 @@
 import assert from "node:assert"
 import {it, describe} from "node:test"
+import {getFizzBuzzResult} from "./index.js"
 
 describe("TDD example", () => {
-    it("", () => {
-        assert.equal(1, 1)
+    it("should get correct FizzBuzz result for input 1", () => {
+        const input = [1]
+        const result = getFizzBuzzResult(input)
+        assert.deepStrictEqual(result, "1")
     })
 })
 
