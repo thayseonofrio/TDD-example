f30512d3020e4336d3cd1812d5a9c878a3b70094
Thayse <thayseonofrio@hotmail.com>
2023-07-03 09:56:23 -0300

    refactor: use more descriptive names for fizzbuzz tests

--- test.js
+++ test.js
@@ -3,22 +3,28 @@ import { it, describe } from "node:test";
 import { getFizzBuzzResult } from "./index.js";
 
 describe("TDD example", () => {
-  it("should get correct FizzBuzz result for input 1", () => {
+  it("should get number 1 result for input 1", () => {
     const input = [1];
     const result = getFizzBuzzResult(input);
     assert.deepStrictEqual(result, "1");
   });
 
-  it("should get correct FizzBuzz result for input 1, 2, 3", () => {
-    const input = [1, 2, 3];
+  it("should get Fizz in place of input 3", () => {
+    const input = [3];
     const result = getFizzBuzzResult(input);
-    assert.deepStrictEqual(result, "1,2,Fizz");
+    assert.deepStrictEqual(result, "Fizz");
   });
 
-  it("should get correct FizzBuzz result for input 1, 2, 3, 4, 5", () => {
-    const input = [1, 2, 3, 4, 5];
+  it("should get Buzz in place of input 5", () => {
+    const input = [5];
     const result = getFizzBuzzResult(input);
-    assert.deepStrictEqual(result, "1,2,Fizz,4,Buzz");
+    assert.deepStrictEqual(result, "Buzz");
+  });
+
+  it("should get FizzBuzz in place of input 15", () => {
+    const input = [15];
+    const result = getFizzBuzzResult(input);
+    assert.deepStrictEqual(result, "FizzBuzz");
   });
 
   it("should get correct FizzBuzz result for input 1, 2, 3, 4, 5, 10, 15", () => {
